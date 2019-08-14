37eece05e38f40305b6853d17201958ee1f93092
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-08-14 13:31:15 +0300

    refactoring to multimodule maven project. closes #1

---
+++ pet-clinic-data/src/test/java/guru/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.petclinic;
+
+/**
+ * Created by at on on 14-Aug-19.
+ */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class PetClinicApplicationTests {
+
+//    @Test
+    public void contextLoads() {
+    }
+
+}
+
