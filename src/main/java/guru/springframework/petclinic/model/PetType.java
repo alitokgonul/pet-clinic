f0d1e113eeafaccdc820ccd648e026abae1ddd38
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-08-14 11:58:11 +0300

    adding POJOs. Close number #2.

---
+++ src/main/java/guru/springframework/petclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.petclinic.model;
+
+/**
+ * Created by at on on 14-Aug-19.
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/guru/springframework/petclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.springframework.petclinic.model;
+
+/**
+ * Created by at on on 14-Aug-19.
+ */
+public class Vet extends Person {
+}
