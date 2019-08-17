98e69c2b9309857cfceec068887a4a4dfc789618
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-08-17 13:39:04 +0300

    index page added. close #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by at on on 17-Aug-19.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index.html"})
+    public String index(Model model) {
+
+        return "index";
+    }
+}
