6171b2f114edfb98f6a8b2981c05dc6dd743d337
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-08-23 22:07:52 +0300

    petTypeMapService implemented . close #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.PetType;
+
+/**
+ * Created by at on on 23-Aug-19.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
