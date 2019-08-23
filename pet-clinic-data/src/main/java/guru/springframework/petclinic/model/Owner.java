99f9c98a2c63a163ceccf42de489827f602692ea
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-08-23 21:53:38 +0300

    visit model added . close #25

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by at on on 14-Aug-19.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
