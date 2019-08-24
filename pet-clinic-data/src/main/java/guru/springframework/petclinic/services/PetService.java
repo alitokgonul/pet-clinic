992bba1da591d3a276ee65d229779c63c12c63e5
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-08-24 10:46:08 +0300

    create speciality service . close #40

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetService.java
@@ -1,12 +1,10 @@
 package guru.springframework.petclinic.services;
 
 import guru.springframework.petclinic.model.Pet;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by at on on 14-Aug-19.
  */
-@Service
 public interface PetService extends CrudService<Pet, Long>{
 
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Speciality;
+
+/**
+ * Created by at on on 24-Aug-19.
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
