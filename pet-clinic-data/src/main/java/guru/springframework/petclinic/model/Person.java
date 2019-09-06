948c73e436275e3385bc932205174927bf52ebe8
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-09-06 14:18:55 +0300

    convert owner to JPA entites . close #35

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Person.java
@@ -1,11 +1,18 @@
 package guru.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by at on on 14-Aug-19.
  */
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/PetType.java
@@ -1,10 +1,17 @@
 package guru.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by at on on 14-Aug-19.
  */
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
