eec008ea718b47a2db2a3e4abe6bc0b5fdd3e87b
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-09-07 20:37:19 +0300

    visit service implemented. close #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Visit;
+
+/**
+ * Created by at on on 07-Sep-19.
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
