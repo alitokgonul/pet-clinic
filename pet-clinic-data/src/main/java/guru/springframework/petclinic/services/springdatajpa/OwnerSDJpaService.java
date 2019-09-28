7ba056226fc6f27332c8a8ab2bcec78672e64c9f
DESKTOP-RLKJTO7\ali <alitokgonul@gmail.com>
2019-09-28 18:38:42 +0300

    ownerRepository MocitoTest done. close #50

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -2,6 +2,8 @@ package guru.springframework.petclinic.services.springdatajpa;
 
 import guru.springframework.petclinic.model.Owner;
 import guru.springframework.petclinic.repositories.OwnerRepository;
+import guru.springframework.petclinic.repositories.PetRepository;
+import guru.springframework.petclinic.repositories.PetTypeRepository;
 import guru.springframework.petclinic.services.OwnerService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,14 @@ import java.util.Set;
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
 
-    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
         this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
     }
-
 
     @Override
     public Owner findByLastName(String lastName) {
